cde0c8f1c8fe728a16f06821d391e693dc5faeb1
heryanq <heryan_@hotmail.com>
2019-11-11 21:31:31 -0300

    Precisa estilizar e implementar algumas funções ainda

---
+++ client/src/components/Login.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-toolbar
+              color="primary"
+              dark
+              flat>
+              <v-toolbar-title>
+                Login
+              </v-toolbar-title>
+              <v-spacer />
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  label="Email"
+                  name="email"
+                  prepend-icon="email"
+                  type="text"/>
+              
+                <v-text-field
+                  id="password"
+                  label="Password"
+                  name="password"
+                  prepend-icon="lock"
+                  type="password"/>
+                <br>
+                <div class="danger-alert" v-html="error" />
+                <br>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                dark
+                @click="login">
+                Login
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        const response = await AuthenticationService.login({
+          email: this.email,
+          password: this.password
+        })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
+        this.$router.push({
+          name: 'songs'
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.container.fluid {
+  max-width: 50%;
+}
+
+</style>
---
+++ client/src/components/Register.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-toolbar
+              color="primary"
+              dark
+              flat>
+              <v-toolbar-title>
+                Register
+              </v-toolbar-title>
+              <v-spacer />
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  label="Email"
+                  name="email"
+                  prepend-icon="email"
+                  type="text"/>
+              
+                <v-text-field
+                  id="password"
+                  label="Password"
+                  name="password"
+                  prepend-icon="lock"
+                  type="password"/>
+                <br>
+                <div class="danger-alert" v-html="error" />
+                <br>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                class="v-btn v-btn--rounded v-btn--loading theme--light elevation-0 v-size--large accent-4"
+                dark
+                @click="login">
+                Register
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'songs'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container.fluid{
+  max-width: 50%;
+}
+.home {
+  cursor: pointer;
+}
+.home:hover {
+  color: red;
+}
+</style>
